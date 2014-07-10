11649f7f41807ac509d8aadbc48623242d6ed84d
JB MONIN <jb.monin@cleo-consulting.fr>
2014-07-10 14:38:46 +0200

    Ajout des methods rowToObject dans les mappers

--- src/application/Core/models/mappers/Article.php
+++ src/application/Core/models/mappers/Article.php
@@ -2,44 +2,43 @@
 
 class Core_Model_Mapper_Article 
 {
+	private $dbTable;
+	
+	public function __construct()
+	{
+		$this->dbTable = new Core_Model_DbTable_Article();
+	}
 	
 	public function find($id) 
 	{
-		// ARTICLE
-		$dbTableArticle = new Core_Model_DbTable_Article();
-		$resultArticle = $dbTableArticle->find($id)->current();
-		$resultCategorie= $resultArticle->findParentRow('Core_Model_DbTable_Categorie');
-		
-		$article = new Core_Model_Article;
-		$article->setId($id)
-		->setTitle($resultArticle['article_title'])
-		->setContent($resultArticle['article_content']);
-		
-		// CATEGORIE
-		$categorie = new Core_Model_Categorie;
-		$categorie->setId($resultCategorie['categorie_id'])
-		->setNom($resultCategorie['categorie_nom']);
-		
-		$categorie->addArticle($article);
-		$article->setCategorie($categorie);
-		
+		$rowArticle = $this->dbTable->find($id)->current();
+		$article = $this->rowToObject($rowArticle);
 		return $article;
 	}
 	
 	public function fetchAll($where=null, $order=null, $count=null, $offset=null) 
 	{
-		$dbTable = new Core_Model_DbTable_Article();
-		$result = $dbTable->fetchAll($where,$order, $count, $offset);
-		
-		$articles = array();
-		foreach ($result as $row) {
-			$article = new Core_Model_Article;
-			$article->setId($row['article_id'])
-			->setTitle($row['article_title'])
-			->setContent($row['article_content']);
-			$articles[] = $article;
+		$rowset = $this->dbTable->fetchAll($where,$order, $count, $offset);
+		$articles = array(); 
+		foreach ($rowset as $row) {
+			$articles[] = $this->rowToObject($row);
 		}
-		
 		return $articles;
+	}
+	
+	public function rowToObject(Zend_Db_Table_Row $row)
+	{
+		$article = new Core_Model_Article;
+		$article->setId($row['article_id'])
+				->setTitle($row['article_title'])
+				->setContent($row['article_content']);
+		
+		$rowCategorie= $row->findParentRow('Core_Model_DbTable_Categorie');
+		$mapperCategorie = new Core_Model_Mapper_Categorie();
+		$categorie = $mapperCategorie->rowToObject($rowCategorie);
+		
+		$categorie->addArticle($article);
+		$article->setCategorie($categorie);
+		return $article;
 	}
 }
--- src/application/Core/models/mappers/Categorie.php
+++ src/application/Core/models/mappers/Categorie.php
@@ -1,1 +1,20 @@
-<?php ?>
+<?php 
+
+class Core_Model_Mapper_Categorie
+{
+	private $dbTable;
+	
+	public function __construct()
+	{
+		$this->dbTable = new Core_Model_DbTable_Categorie();
+	}
+	
+	public function rowToObject(Zend_Db_Table_Row $row)
+	{
+		$categorie = new Core_Model_Categorie;
+		$categorie->setId($row['categorie_id'])
+				  ->setNom($row['categorie_nom']);
+		
+		return $categorie;
+	}
+}
